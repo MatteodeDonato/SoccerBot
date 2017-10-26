7d5a789ba8c481e4453686ec8bcfe4b8ffbd9fdc
david115599 <davidbershadsky@RaspberryPi-2.local>
2017-10-26 11:30:45 -0400

    S
    S

--- Brushless_test/Brushless_test.ino
+++ Brushless_test/Brushless_test.ino
@@ -1,15 +1,30 @@
-#include <Servo.h>//Using servo library to control ESC
-Servo esc; //Creating a servo class with name as esc
-void setup()
-{
-esc.attach(9); //Specify the esc signal pin,Here as D8
-esc.writeMicroseconds(1000); //initialize the signal to 1000
+#include <Servo.h>
+
+byte servoPin4 = 5;
+byte servoPin1 = 2;
+byte servoPin2 = 3;
+byte servoPin3 = 4;
+Servo servo;
+
+void setup() {
+    servo.attach(servoPin4);
 Serial.begin(9600);
+servo.writeMicroseconds(1500); // send "stop" signal to ESC.
+  delay(5000); // delay to allow the ESC to recognize the stopped signal
 }
-void loop()
-{
-int val; //Creating a variable val
-val= 2000; //Read input from analog pin a0 and store in val
-val= map(val, 0, 1023,1000,2000); //mapping val to minimum and maximum(Change if needed) 
-esc.writeMicroseconds(val); //using val as the signal to esc
+
+void loop() {
+  int signal = 1900; // Set signal value, which should be between 1100 and 1900
+  servo.writeMicroseconds(signal); // Send signal to ESC.
+  delay(5000);
+Serial.print("HI");
 }
+
+void North(){}
+void South(){}
+void East(){}
+void West(){}
+void NorthEast(){}
+void NorthWest(){}
+void SouthEast(){}
+void SouthWest(){}
